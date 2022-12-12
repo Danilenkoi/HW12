8d4b37b67b71638e600f980dc1fca81ebb74fd39
iladanilenko <Danilenkoi@inbox.ru>
2022-12-12 16:18:08 +0300

    Сдача домашнего задания HW-12

---
+++ src/Author.java
@@ -0,0 +1,12 @@
+public class Author {
+
+    public String name;
+
+    public Author(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
 
+        Book sKing = new Book(2019, "The Institute");
+        Author king = new Author("S. King");
+        System.out.println("Автор - " + king.getName());
+        System.out.println("Год издания - " + sKing.getYear());
+        System.out.println("Название - " + sKing.getTitle());
+        sKing.setYear(2020);
+        System.out.println("Год переиздания - " + sKing.getYear());
+
+        Book sH = new Book( 2017, "A brief history of time");
+        Author hw = new Author("S. Hawking");
+        System.out.println("Автор - " + hw.getName());
+        System.out.println("Год издания - " + sH.getYear());
+        System.out.println("Название - " + sH.getTitle());
     }
 }
